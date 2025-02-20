c9f62233dd335f59f4bbbf27602ec0ee6a028a04
Siboniso <sibandzesiboniso0@gmail.com>
2025-02-20 21:42:14 +0200

    update image URLs and paths to reflect new directory structure

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
         <a class="navbar-brand" style="margin-left:50px;" href="auction.gaadiexpert.com"></a>
         <div style="display:flex; justify-content: left;   position: absolute;
   right: 0px;">
-        <img class="img-responsive" src="https://liquag.com/dev/rail2/images/logo1.png" style="display:flex; justify-content: left; margin-top:2px; width:180px;  ">
+        <img class="img-responsive" src="https://liquag.com/dev/rail3/images/logo1.png" style="display:flex; justify-content: left; margin-top:2px; width:180px;  ">
      
         </div>
         <div class="menu">
@@ -13,27 +13,27 @@
 	            <ul class="list-unstyled">
             <li><a href="home.php" class="list-group-item"><i class="fa fa-tachometer"></i> Dashboard <span class="badge"></span> </a></li>
 
-            <li><a href="https://liquag.com/dev/rail2/reports.php" class="list-group-item"><i class="fa fa-file"></i> Reports</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/reports.php" class="list-group-item"><i class="fa fa-file"></i> Reports</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/constants.php" class="list-group-item"><i class="fa fa-gear"></i> Constants</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/constants.php" class="list-group-item"><i class="fa fa-gear"></i> Constants</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/tenants.php" class="list-group-item"><i class="fa fa-list"></i>  All tenants</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/tenants.php" class="list-group-item"><i class="fa fa-list"></i>  All tenants</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/houses2.php" class="list-group-item"><i class="fa fa-home"></i> Houses Data</a> </li>
-             <li><a href="https://liquag.com/dev/rail2/faults.php" class="list-group-item"><i class="fa fa-money"></i> Report Faults</a> </li>
-            <li><a href="https://liquag.com/dev/rail2/house_profile.php" class="list-group-item"><i class="fa fa-money"></i> House Profile</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/houses2.php" class="list-group-item"><i class="fa fa-home"></i> Houses Data</a> </li>
+             <li><a href="https://liquag.com/dev/rail3/faults.php" class="list-group-item"><i class="fa fa-money"></i> Report Faults</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/house_profile.php" class="list-group-item"><i class="fa fa-money"></i> House Profile</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/billing.php" class="list-group-item"><i class="fa fa-file"></i> Billing</a> </li>
-            <li><a href="https://liquag.com/dev/rail2/statement.php" class="list-group-item"><i class="fa fa-file"></i> Statements</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/billing.php" class="list-group-item"><i class="fa fa-file"></i> Billing</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/statement.php" class="list-group-item"><i class="fa fa-file"></i> Statements</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/payment.php" class="list-group-item"><i class="fa fa-money"></i> Process Payments</a> </li>
-             <li><a href="https://liquag.com/dev/rail2/bulkpayment.php" class="list-group-item"><i class="fa fa-money"></i> Bulk Payments</a> </li>
-              <li><a href="https://liquag.com/dev/rail2/users.php" class="list-group-item"><i class="fa fa-money"></i> Users</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/payment.php" class="list-group-item"><i class="fa fa-money"></i> Process Payments</a> </li>
+             <li><a href="https://liquag.com/dev/rail3/bulkpayment.php" class="list-group-item"><i class="fa fa-money"></i> Bulk Payments</a> </li>
+              <li><a href="https://liquag.com/dev/rail3/users.php" class="list-group-item"><i class="fa fa-money"></i> Users</a> </li>
 
            
     
 
-            <li><a href="https://liquag.com/dev/rail2/script/logout.php" class="list-group-item"><i class="fa fa-power-off"></i> Log Out</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/script/logout.php" class="list-group-item"><i class="fa fa-power-off"></i> Log Out</a> </li>
             </ul>
          </div>
         </div>
--- payments/header.php
+++ payments/header.php
@@ -5,7 +5,7 @@
         <a class="navbar-brand" style="margin-left:50px;" href="auction.gaadiexpert.com"></a>
         <div style="display:flex; justify-content: left;   position: absolute;
   right: 0px;">
-        <img class="img-responsive" src="https://liquag.com/dev/rail2/images/logo1.png" style="display:flex; justify-content: left; margin-top:2px; width:180px;  ">
+        <img class="img-responsive" src="https://liquag.com/dev/rail3/images/logo1.png" style="display:flex; justify-content: left; margin-top:2px; width:180px;  ">
      
         </div>
         <div class="menu">
@@ -13,7 +13,7 @@
 	            <ul class="list-unstyled">
             <li><a href="payments.php" class="list-group-item"><i class="fa fa-money"></i> Process Invoice</a> </li>
 
-            <li><a href="https://liquag.com/dev/rail2/payments/logout.php" class="list-group-item"><i class="fa fa-power-off"></i> Log Out</a> </li>
+            <li><a href="https://liquag.com/dev/rail3/payments/logout.php" class="list-group-item"><i class="fa fa-power-off"></i> Log Out</a> </li>
             </ul>
          </div>
         </div>
